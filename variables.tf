568d36ac92475926f3846175254babe3f5c20c3a
Shey Njila <sheynjila.devtest@gmail.com>
2022-12-06 14:13:09 -0500

    Editing root module path

--- variables.tf
+++ variables.tf
@@ -4,16 +4,16 @@ variable "access_ip" {
   type = string
 }
 
-variable "db_name" {
-  type = string
-}
+# variable "db_name" {
+#   type = string
+# }
 
-variable "dbuser" {
-  type      = string
-  sensitive = true
-}
+# variable "dbuser" {
+#   type      = string
+#   sensitive = true
+# }
 
-variable "dbpassword" {
-  type      = string
-  sensitive = true
-}
+# variable "dbpassword" {
+#   type      = string
+#   sensitive = true
+# }
