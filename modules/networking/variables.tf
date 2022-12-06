12a6a0930ae3510b7230a0581ed8eb93cc22eb91
Shey Njila <sheynjila.devtest@gmail.com>
2022-12-06 14:33:53 -0500

    Editing root module path

--- modules/networking/variables.tf
+++ modules/networking/variables.tf
@@ -1,8 +1,8 @@
 # --- networking/variables.tf ---
 
-# variable "vpc_cidr" {
-#   type = string
-# }
+variable "vpc_cidr" {
+  type = string
+}
 
 variable "public_sn_count" {
   type = number
@@ -12,10 +12,10 @@ variable "private_sn_count" {
   type = number
 }
 
-variable "access_ip" {
-  default = "96.32.151.128/32"
-  type = string
-}
+# variable "access_ip" {
+#   default = "96.32.151.128/32"
+#   type = string
+# }
 
 variable "db_subnet_group" {
   type = bool
